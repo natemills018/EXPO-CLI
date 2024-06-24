3a91ff3416dd93c7dabda9f715f9eba76a700b90
Nate Mills <natemills018@gmail.com>
2024-06-23 20:35:59 -0400

    Messing around, completing lab

--- App.tsx
+++ App.tsx
@@ -3,18 +3,40 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.redContainer}>
+      <View style={styles.blackContainer}>
+        <View style={styles.container}>
+          <Text style={styles.textStyle}>#crushing it</Text>
+          <StatusBar style="auto" />
+        </View>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  redContainer: {
+    flex: 1,
+    backgroundColor: 'red',
+    padding: 20
+  },
+
+  blackContainer: {
+    flex: 1,
+    backgroundColor: 'black',
+    padding: 10
+  },
+
   container: {
     flex: 1,
-    backgroundColor: '#0091ea',
+    backgroundColor: '#90EE90',
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  textStyle: {
+    fontSize: 40,
+    backgroundColor: 'red',
+    color: 'white'
+  }
 });
